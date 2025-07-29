6d49776770e9f9dd817fbeea1b91acae0ff9e07c
Iced Tea <139998388+icd-t@users.noreply.github.com>
2025-07-29 07:40:06 -0500

    Update 4.0.0

--- scripts/daysSince.js
+++
@@ -1,8 +0,0 @@
-window.onload=function() {
-    const formatter = new Intl.RelativeTimeFormat('en');
-    var dateIn = document.getElementById('dateDisplay').innerHTML;
-    const diff = new Date() - new Date(dateIn);
-    const daysSince = formatter.format(Math.round(-diff / (1000*60*60*24)), 'days');
-    
-    document.getElementById("days").innerHTML = daysSince;
-}
